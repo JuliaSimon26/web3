391b6a20bfa72c09acd4cc73876afb0950e444d8
Julia <madam.julia.26@yandex.ru>
2020-04-08 16:44:55 +0300

    cycle

--- 3.js
+++ 3.js
@@ -15,7 +15,19 @@ const object3 = {
 
 // Реализуйте функцию isEquivalent
 function isEquivalent(a, b) {
-    return (a.name === b.name && a.weight === b.weight) 
+	var a_Props = Object.getOwnPropertyNames(a);
+    var b_Props = Object.getOwnPropertyNames(b);
+    if (a_Props.length != b_Props.length) { //если не совпадает длина - сразу останавливаемся
+        return false;
+    }
+
+    for (var i = 0; i < a_Props.length; i++) {
+        var propName = a_Props[i];
+        if (a[propName] !== b[propName]) {
+            return false; 
+        }
+	}
+	return true;
 }
 console.log(isEquivalent(object1, object2)); // true
 console.log(isEquivalent(object1, object3)); // false
